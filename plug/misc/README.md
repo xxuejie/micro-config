f943855ce281b91355f8e425a8175c7237500842
Xuejie Xiao <xxuejie@gmail.com>
2021-09-13 03:13:31 +0000

    Add completion util

---
+++ plug/misc/README.md
@@ -0,0 +1,5 @@
+# micro-misc
+
+Some miscellaneous utils that I use in micro editor, but might not be generic enough for a separate plugin.
+
+To use this, you need to add the `bin` folder within the plugin to your `PATH`.
---
+++ plug/misc/main.lua
@@ -0,0 +1,53 @@
+VERSION = "0.0.1"
+
+local micro = import("micro")
+local buffer = import("micro/buffer")
+local shell = import("micro/shell")
+local strings = import("strings")
+
+function complete(_pane, _args)
+    local v = micro.CurPane()
+
+    if v.Cursor == nil then
+        return
+    end
+
+    local line = v.Buf:Line(v.Cursor.Loc.Y)
+    local s1 = string.sub(line, 1, v.Cursor.Loc.X)
+    local m1 = string.match(s1, "[^%s]*$")
+
+    _runComplete(m1, v.Buf.Path, {v, buffer.Loc(v.Cursor.X, v.Cursor.Y)})
+end
+
+function _runComplete(prefix, filename, runargs)
+    local bp = runargs[1]
+    local prefixCommand = ""
+    if prefix ~= "" then
+         prefixCommand = "--prefix " .. prefix
+    end
+    local command = "bash -l -c 'generate_completion " .. prefixCommand .. " " .. filename .. " | fzf'"
+
+    if shell.TermEmuSupported then
+        local err = shell.RunTermEmulator(bp, command, false, true, _completeOutput, runargs)
+        if err ~= nil then
+            micro.InfoBar():Error(err)
+        end
+    else
+        local output, err = shell.RunInteractiveShell(command, false, true)
+        if err ~= nil then
+            micro.InfoBar():Error(err)
+        else
+            _completeOutput(output, runargs)
+        end
+    end
+end
+
+function _completeOutput(output, runargs)
+    local bp = runargs[1]
+    local loc = runargs[2]
+
+    output = strings.TrimSpace(output)
+    if output ~= "" then
+        bp.Buf:Replace(loc, loc, output)
+    end
+end
