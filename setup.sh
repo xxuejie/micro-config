940de5af761e40b9c3f50e1a06a3b8e2b57295e7
Xuejie Xiao <xxuejie@gmail.com>
2026-03-10 06:15:39 +0000

    Update setup script with fzf note

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 micro -plugin install fzf
+
+echo "Ensure fzf and ag are installed, and the following line is in your shell config:"
+echo "export FZF_DEFAULT_OPTS=\"--color=dark --no-scrollbar --pointer='> ' --marker='* ' --gutter=' '\""
